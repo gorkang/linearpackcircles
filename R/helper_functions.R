73e3b173680a51ed1898ce9aecd227054d97d49d
G <gorkang@gmail.com>
2021-08-18 12:14:39 +0100

    first version of packing circles for owid data

---
+++ R/helper_functions.R
@@ -0,0 +1,96 @@
+create_polygons <- function(DF) {
+  
+  library(packcircles)
+  
+  limits = c(min(DF$x, na.rm = TRUE), max(DF$x, na.rm = TRUE))
+  limits_y = c(min(DF$y, na.rm = TRUE), max(DF$y, na.rm = TRUE)) 
+  
+  res <- circleRepelLayout(DF, xlim = limits, ylim = limits_y, xysizecols = 1:3)
+  cat(res$niter, "iterations performed")
+  
+  # Get vertex data for the initial layout where sizes are areas
+  dat.gg.initial <- circleLayoutVertices(DF, sizetype = "area")
+  
+  # Get vertex data for the layout returned by the function where sizes are radii
+  dat.gg.final <- circleLayoutVertices(res$layout)
+  
+  DF_output = dat.gg.final %>% left_join(DF %>% mutate(id = 1:n()) %>% select(id, location, continent, total_cases_per_million), by = "id")
+  
+  return(DF_output)
+  
+}
+
+
+check_diffs <- function(ALL_data, DF) {
+  
+  # Extract continent
+  continent_str = DF %>% distinct(continent) %>% pull(continent)
+  
+  DFCHECK = 
+    DF %>% group_by(location) %>% sample_n(1) %>% arrange(x) %>% ungroup() %>% 
+    mutate(position_check = 1:n(), total_cases_plot = x * ratio_reduction_x) %>% 
+    rename(x_check = x) %>% 
+    select(position_check, x_check, location, total_cases_plot)
+  
+  DF_output = 
+    ALL_data %>% filter(continent == continent_str) %>% 
+    arrange(x) %>% ungroup() %>% mutate(position_x = 1:n()) %>% 
+    select(x, location, continent, position_x, total_cases_per_million) %>% 
+    left_join(DFCHECK, by = "location") %>%
+    mutate(DIFF = position_x - position_check,
+           DIFF_n = (total_cases_plot - total_cases_per_million),
+           DIFF_pct = ((total_cases_plot - total_cases_per_million)/total_cases_per_million) * 100,
+           DIFF_abs = ((total_cases_plot - total_cases_per_million)/max(total_cases_per_million)) * 100) %>%
+    select(DIFF, matches("position_"), starts_with("x"), everything())
+  
+  count_output = 
+    DF_output %>% 
+    group_by(DIFF) %>% 
+    summarise(N = n(),
+              MEAN = mean(DIFF_n),
+              MAX = max(DIFF_n),
+              PCT = mean(DIFF_pct),
+              PCT_abs = mean(DIFF_abs),
+              MAX_PCT_abs = max(DIFF_abs),
+              continent = unique(continent)
+    )
+  
+  list_output = list(DF_output = DF_output, count_output = count_output)
+  
+  return(list_output)
+  
+}
+
+
+# Return x axis labels to original values (we use ratio_reduction_x above to be able to perform calculations)
+mult_format <- function() {
+  function(x) format(ratio_reduction_x * x, digits = 2) %>% as.numeric() %>% scales::comma()
+}
+
+
+create_plot <- function(DF, label_positions = NA) {
+  
+  plot1 = ggplot(data = DF, aes(x, y, group = id)) +
+    geom_polygon(colour = "#2b695c", fill = "#b8ceca", alpha = 0.3) +
+    coord_equal() +
+    labs(x = "", y = DF %>% distinct(continent)) +
+    theme_minimal() +
+    scale_x_continuous(labels = mult_format(), n.breaks = 10)  +
+    theme(axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())
+  
+  if (!is.null(nrow(label_positions))) {
+    plot1 + 
+      # ggrepel::geom_label_repel(
+      ggrepel::geom_text_repel(
+        size = 2,
+        max.overlaps = 5, 
+        # nudge_x = .1, nudge_y = .5,
+        alpha = .5,
+        data = label_positions, 
+        aes(label = paste0(location, ": ", round(total_cases_per_million,0) %>% scales::comma())), size = size_text
+      ) 
+  } else {
+    plot1
+  }
+}
---
+++ owid.R
@@ -0,0 +1,68 @@
+# Crea una visualizacion de packing circles para datos covid. 
+
+  # TODO: generalizar para cualquier tipo de datos!
+
+# Libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(patchwork)
+library(packcircles)
+library(ggplot2)
+
+lapply(list.files("./R", full.names = TRUE, pattern = ".R"), source)
+
+
+# Parameters --------------------------------------------------------------
+
+set.seed(12)
+height_y = 2
+ratio_reduction_x = 10000
+size_text = 3
+
+
+# Data preparation --------------------------------------------------------
+
+DF = read_csv("data/owid-covid-data.csv")
+
+ALL_data = 
+  DF %>% 
+  filter(date == max(date)) %>% 
+  select(location, continent, total_cases_per_million, total_deaths_per_million) %>% 
+  drop_na(continent) %>% 
+  drop_na() %>% 
+  mutate(area = (total_deaths_per_million)/ratio_reduction_x,
+         x = (total_cases_per_million)/ratio_reduction_x,
+         y = runif(n(), 1, height_y)) %>% 
+  select(x, y, area, location, continent, total_cases_per_million)  
+
+
+# Create polygons ---------------------------------------------------------
+
+DF1 = create_polygons(ALL_data %>% filter(continent == "Asia")) %>% mutate(XX = "Asia")
+DF2 = create_polygons(ALL_data %>% filter(continent == "Europe")) %>% mutate(XX = "Europe")
+
+# Position of text labels
+label_positions1 = DF1 %>% group_by(id)  %>% filter(y == max(y)) %>% filter(x == median(x))%>% sample_n(1)
+label_positions2 = DF2 %>% group_by(id)  %>% filter(y == max(y)) %>% filter(x == median(x))%>% sample_n(1)
+
+
+
+# CHECKS -------------------------------------------------------------------
+
+check_diffs(ALL_data, DF1)
+check_diffs(ALL_data, DF2)
+
+
+# PLOT --------------------------------------------------------------------
+
+plot1 = create_plot(DF1, label_positions1) + labs(title = "COVID cases per million by continent")
+plot2 = create_plot(DF2, label_positions2) + labs(x = "Cases per million", caption = "Diameter is deaths per million \n Data from https://github.com/owid/covid-19-data")
+
+
+# https://stackoverflow.com/questions/60172472/plotting-ggplot2-geom-polygon-on-discrete-x-axis
+final_plot = plot1 / plot2
+final_plot
+
+ggsave("outputs/final_plot.png", final_plot, width = 20, height = 10, dpi = 300)
