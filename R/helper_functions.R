c81b2278335c1529b9561befb4014873906f46c9
G <gorkang@gmail.com>
2021-08-19 11:58:02 +0100

    clean up. more parametrization. Fix bugs

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,13 +1,39 @@
-create_polygons <- function(DF) {
+prepare_data <- function(DF, ID_var, group_var, area_var, x_var) {
   
+  # DEBUG
+  # DF = DF
+  # ID_var = "location"
+  # group_var = "continent"
+  # area_var = "total_cases_per_million"
+  # x_var = "total_deaths_per_million"
+  
+  DF_prepared_data = 
+    DF %>% 
+    select(all_of(ID_var), all_of(group_var), all_of(area_var), all_of(x_var)) %>% 
+    drop_na() %>% 
+    mutate(area = get(area_var)/ratio_reduction_area,
+           x = get(x_var)/ratio_reduction_x,
+           y = runif(n(), 0, height_y)) %>%
+    select(x, y, area, all_of(ID_var), all_of(group_var), all_of(area_var), all_of(x_var))  
+  
+  return(DF_prepared_data)
+  
+}
+
+
+
+
+create_polygons <- function(DF, group_var) {
+  
+  # DEBUG
   # DF = ALL_data %>% filter(continent == DF_CONTINENTS$continent[1])
   
   library(packcircles)
   
-  limits = c(min(DF$x, na.rm = TRUE), max(DF$x, na.rm = TRUE))
+  limits_x = c(min(DF$x, na.rm = TRUE), max(DF$x, na.rm = TRUE))
   limits_y = c(min(DF$y, na.rm = TRUE), max(DF$y, na.rm = TRUE)) 
   
-  res <- circleRepelLayout(DF, xlim = limits, ylim = limits_y, xysizecols = 1:3)
+  res <- circleRepelLayout(DF, xlim = limits_x, ylim = limits_y, xysizecols = 1:3, wrap = FALSE)
   cat("- ", res$niter, "iterations performed\n")
   
   # Get vertex data for the initial layout where sizes are areas
@@ -16,35 +42,44 @@ create_polygons <- function(DF) {
   # Get vertex data for the layout returned by the function where sizes are radii
   dat.gg.final <- circleLayoutVertices(res$layout)
   
-  DF_output = dat.gg.final %>% left_join(DF %>% mutate(id = 1:n()) %>% select(id, location, continent, total_cases_per_million), by = "id") %>% 
-    mutate(id = paste0(id, "_", continent))
+  # Join original data with generated data
+  names_DF = DF %>% select(-x, -y, -area) %>% names(.)
+  
+  DF_output = dat.gg.final %>% left_join(DF %>% mutate(id = 1:n()) %>% select(id, all_of(names_DF)), by = "id") %>% 
+    mutate(id = paste0(id, "_", get(group_var)))
   
   return(DF_output)
   
 }
 
 
-check_diffs <- function(ALL_data, DF) {
+
+check_diffs <- function(ALL_data, DF, check_var) {
+  
+  # DEBUG
+  # DF = DF1
+  # check_var = "total_deaths_per_million"
   
   # Extract continent
   continent_str = DF %>% distinct(continent) %>% pull(continent)
   
   DFCHECK = 
     DF %>% group_by(location) %>% sample_n(1) %>% arrange(x) %>% ungroup() %>% 
-    mutate(position_check = 1:n(), total_cases_plot = x * ratio_reduction_x) %>% 
+    mutate(position_check = 1:n(), total_x_plot = x * ratio_reduction_x) %>% 
     rename(x_check = x) %>% 
-    select(position_check, x_check, location, total_cases_plot)
+    select(position_check, x_check, location, total_x_plot)
   
   DF_output = 
     ALL_data %>% filter(continent == continent_str) %>% 
-    arrange(x) %>% ungroup() %>% mutate(position_x = 1:n()) %>% 
-    select(x, location, continent, position_x, total_cases_per_million) %>% 
+    arrange(x) %>% ungroup() %>% mutate(position_x = 1:n()) %>% # arrange() needed to assign position_x
+    select(x, location, continent, position_x, eval(check_var)) %>% 
     left_join(DFCHECK, by = "location") %>%
     mutate(DIFF = position_x - position_check,
-           DIFF_n = (total_cases_plot - total_cases_per_million),
-           DIFF_pct = ((total_cases_plot - total_cases_per_million)/total_cases_per_million) * 100,
-           DIFF_abs = ((total_cases_plot - total_cases_per_million)/max(total_cases_per_million)) * 100) %>%
-    select(DIFF, matches("position_"), starts_with("x"), everything())
+           DIFF_n = (total_x_plot - get(check_var)),
+           DIFF_pct = ((total_x_plot - get(check_var))/get(check_var)) * 100,
+           DIFF_abs = ((total_x_plot - get(check_var))/max(get(check_var))) * 100) %>%
+    select(DIFF, matches("position_"), starts_with("x"), everything()) %>% 
+    arrange(DIFF)
   
   count_output = 
     DF_output %>% 
@@ -71,40 +106,46 @@ mult_format <- function() {
 }
 
 
-create_plot <- function(DF, label_circles = FALSE) {
+create_plot <- function(DF, label_circles = FALSE, max_overlaps = 5) {
   
   # DF = DFX
   # label_circles = TRUE
   
-  mean_Y = DF %>% group_by(continent) %>% summarise(XXX = median(y)) %>% arrange(XXX)
+  # Position for y axis labels (group_var)
+  position_y = DF %>% group_by(continent) %>% summarise(positions = median(y)) %>% arrange(positions)
   
-  # Position of text labels
-  label_positions = DF %>% group_by(id)  %>% filter(y == max(y)) %>% filter(x == median(x)) %>% sample_n(1)
+  # Position of circle labels
+  label_positions = DF %>% group_by(id)  %>% filter(y == median(y)) %>% filter(x == median(x)) %>% sample_n(1)
   
-  
+  # Main plot
   plot1 = 
     ggplot(data = DF, aes(x, y, group = id)) +
     geom_polygon(colour = "#2b695c", fill = "#b8ceca", alpha = 0.3) +
     coord_equal() +
     labs(x = "", y = "") +
-    theme_minimal() +
-    scale_x_continuous(labels = mult_format(), n.breaks = 10)  +
-    # theme(axis.text.y=element_blank(),
-    #       axis.ticks.y=element_blank()) +
-    scale_y_continuous(breaks = unique(mean_Y$XXX), labels = unique(DF$continent))
+    theme_minimal(base_size = 16) +
+    scale_x_continuous(labels = mult_format(), n.breaks = 10, expand = expansion(mult = c(.02, .01)))  +
+    theme(plot.background = element_rect(fill = 'white', colour = 'white')) +
+    scale_y_continuous(breaks = unique(position_y$positions), labels = unique(DF$continent))
   
-  
+  # If including labels for circles
   if (label_circles) {
     plot1 + 
       # ggrepel::geom_label_repel(
       ggrepel::geom_text_repel(
-        size = 2,
-        max.overlaps = 5, 
+        size = size_text,
+        max.overlaps = max_overlaps, 
+        max.time = .5, max.iter = 10000000,
+        force_pull = 1,
+        force = 1,
         # nudge_x = .1, nudge_y = .5,
         alpha = .5,
         data = label_positions, 
-        aes(label = paste0(location, ": ", round(x * ratio_reduction_x,0) %>% scales::comma())), size = size_text #total_cases_per_million
-      ) 
+        aes(label = paste0(location#, ": ", 
+                           # round(x * ratio_reduction_x, 0) %>% scales::comma(accuracy = 1), " | ",
+                           # round(total_deaths_per_million, 0) %>% scales::comma(accuracy = 1)
+                           ))
+        ) 
   } else {
     plot1
   }
--- owid_IMPROVED.R
+++ owid_IMPROVED.R
@@ -1,18 +1,5 @@
-# Crea una visualizacion de packing circles para datos covid. 
+# Creates a packing circles visualization for COVID OWID data
 
-# TODO: La posicion de algunas labels no esta del todo OK.
-
-# TODO: La label que se muestra es x * ratio_reduction_x. Es la posicion en la que acaba el circulo, no el dato inicial. En ocasiones habra alguna discrepancia.
-
-# TODO: generalizar para cualquier tipo de datos! 
-
-  # Ahora, por ejemplo, si se usa casos por millon para el area, se va al chancho
-    # Habra que normalizar el area tb?
-
-  # mutate(area = (total_cases_per_million)/ratio_reduction_x,
-  #        x = (total_deaths_per_million)/ratio_reduction_x,
-  #        y = runif(n(), 0, height_y)) %>% 
-    
 
 # Libraries ---------------------------------------------------------------
 
@@ -23,20 +10,35 @@ library(purrr)
 library(readr)
 library(tidyr)
 
-lapply(list.files("./R", full.names = TRUE, pattern = ".R"), source)
+invisible(lapply(list.files("./R", full.names = TRUE, pattern = ".R"), source))
 
 
 # Parameters --------------------------------------------------------------
 
 set.seed(12)
-separation_factor = 3
+separation_factor = 10 # Separation between group_var_str levels
 
 ratio_reduction_area = 60000
 ratio_reduction_x = 50
-height_y = 1
-title_str = "COVID cases per million by continent"
+
+height_y = 5
+
+# If using labels, how many max overlaps to tolerate
+max_overlaps = 8
+
+
+ID_var_str = "location"
+group_var_str = "continent"
+area_var_str = "total_cases_per_million"
+x_var_str = "total_deaths_per_million"
+
+
+title_str = "COVID deaths per million"
+subtitle_str = NULL
 x_str = "Deaths per million"
-caption_str = "Diameter is cases per million \n Data from https://github.com/owid/covid-19-data"
+caption_str = "Diameter is cases per million \n Data from https://github.com/owid/covid-19-data \nBy @gorkang"
+size_text = 3
+
 
 # ratio_reduction_area = 10000
 # ratio_reduction_x = 10000
@@ -46,57 +48,55 @@ caption_str = "Diameter is cases per million \n Data from https://github.com/owi
 # caption_str = "Diameter is deaths per million \n Data from https://github.com/owid/covid-19-data"
 
 
-size_text = 3
+
 
 
 # Data preparation --------------------------------------------------------
 
-DF = read_csv("data/owid-covid-data.csv")
+DF = read_csv("data/owid-covid-data.csv", show_col_types = FALSE)
 
-ALL_data = 
-  DF %>% 
-  filter(date == max(date)) %>% 
-  select(location, continent, total_cases_per_million, total_deaths_per_million) %>% 
-  drop_na(continent) %>% 
-  drop_na() %>% 
-  
-  # ESTO DEBERIA ESTAR EN UNA FUNCION -------------
-  # mutate(area = (total_deaths_per_million)/ratio_reduction_area,
-  #        x = (total_cases_per_million)/ratio_reduction_x,
-  #        y = runif(n(), 0, height_y)) %>%
-  
-  mutate(area = (total_cases_per_million)/ratio_reduction_area,
-         x = (total_deaths_per_million)/ratio_reduction_x,
-         y = runif(n(), 0, height_y)) %>%
-  select(x, y, area, location, continent, total_cases_per_million)  
+ALL_data = prepare_data(DF %>% filter(date == max(date)),
+             ID_var = ID_var_str,
+             group_var = group_var_str,
+             area_var = area_var_str,
+             x_var = x_var_str)
+
 
 
 # Create polygons ---------------------------------------------------------
 
-DF_CONTINENTS = ALL_data %>% distinct(continent) 
+DF_CONTINENTS = ALL_data %>% distinct(continent)# %>% head(1)
 
-DF1 = 1:nrow(DF_CONTINENTS) %>% 
-  map_df(~ create_polygons(ALL_data %>% filter(continent == DF_CONTINENTS$continent[.x])))
+DF1 = 
+  1:nrow(DF_CONTINENTS) %>% 
+  map_df(~ create_polygons(ALL_data %>% filter(continent == DF_CONTINENTS$continent[.x]), group_var = "continent"))
 
-
-# CHECKS -------------------------------------------------------------------
-
-check_diffs(ALL_data, DF1 %>% filter(continent == "Asia"))
 
 
 # Separate by factor ------------------------------------------------------
 
+# 
 DF_factors = DF1 %>% distinct(continent) %>% mutate(ID = (1:n()) * separation_factor)
 
-DFX = DF1 %>% left_join(DF_factors, by = "continent") %>% mutate(y = y + ID)
+DFX = DF1 %>% left_join(DF_factors, by = group_var_str) %>% mutate(y = y + ID)
 
-plot_final = create_plot(DFX, label_circles = TRUE)
+
+
+# Plot --------------------------------------------------------------------
+
+plot_final = create_plot(DFX, label_circles = TRUE, max_overlaps = max_overlaps)
 
 final_plot = plot_final +
   labs(title = title_str,
+       subtitle = subtitle_str,
        x = x_str, 
        caption = caption_str)
 
 final_plot
 
-ggsave("outputs/final_plot_improved.png", final_plot, width = 20, height = 10, dpi = 300)
+ggsave("outputs/final_plot_improved.png", final_plot, width = 20, height = 11, dpi = 300)
+
+
+# CHECKS -------------------------------------------------------------------
+
+# check_diffs(ALL_data, DF1 %>% filter(continent == DF_CONTINENTS$continent[4]), check_var = x_var_str)
